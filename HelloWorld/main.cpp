f714b3a911650d7c0e8030ce37a0fcbceb8e2246
fancywuzhe <wuzhe2008@126.com>
2015-05-31 13:11:03 +0800

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by fancy on 15/5/31.
+//  Copyright (c) 2015年 fancy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
