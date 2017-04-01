82832d133d97f65e0d88c08d3e36ef3b732cd4d9
fancywuzhe <wuzhe2008@126.com>
2017-03-31 23:16:06 -0700

    try one
    try one

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,15 +1,1 @@
-
-#include <iostream>
-
-#include <cmath>
-#include "Person.h"
-#include <fstream>
-//show the basic io frome file
-int main(){
-    Time a=Time(2,30);
-    Time b=Time(3,40);
-    Time c=a.Sum(b);
-    a.Show();
-    
-
-}
+my name is wjj
